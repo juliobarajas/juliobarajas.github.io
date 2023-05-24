15c1aa87cba018a2575bbd3caace95b809ba6aef
Julio Barajas <juliob1371@gmail.com>
2023-05-24 01:51:33 -0500

    Updated look of sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import react from 'react';
+import image from './julioimage.jpg';
 import './CSS/App.css';
 import './CSS/Sidebar.css';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="outer-container">
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-      <div id="page-wrap">
+    <div>
+      <div class="header">
         <h1>Personal Website</h1>
-        <h2>Check out the different sections with the sidebar!</h2>
-      </div>  
+        
+      </div>
+      <div class="leftcolumn">
+        <Sidebar></Sidebar>
+      </div>
+      
+      <div class="rightcolumn">
+        This is the right?
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import '../CSS/Sidebar.css';
-import { slide as Menu } from 'react-burger-menu';
-
 
 export default props => {
   return (
-    <Menu width={200}>
-      <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/education">
-        Education
-      </a>
-      <a className="menu-item" href="/experience">
-        Experience
-      </a>
-      <a className="menu-item" href="/projects">
-        Software Projects
-      </a>
-      <a className="menu-item" href="/skills">
-        Skills & Abilities
-      </a>
-      
-    </Menu>
+  <ul>
+    <li><a class="active" href="About">About Me</a></li>
+    <li><a href="Education">Education</a></li>
+    <li><a href="Projects">Personal Projects</a></li>
+    <li><a href="Experience">Experience</a></li>
+    <li><a href="Skills">Skills & Abilities</a></li>
+  </ul>
   );
 };
