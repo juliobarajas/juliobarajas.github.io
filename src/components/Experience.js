db99fe5a3432bfa1c6edc1487a68f6d264cac913
Julio Barajas <juliob1371@gmail.com>
2023-07-13 01:02:28 -0500

    New project link, firefox browser fixes, and added new favicon.ico

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -17,16 +17,20 @@ class Experience extends Component {
                     <h3>What is PeerPresents?</h3>
                     <p>PeerPresents is a web-based system for in-class peer feedback during student presentations. Peer feedback systems enable students to get feedback without substantially burdening the instructor. PeerPresents is a novel system for in-class peer review where students can quickly exchange feedback on projects without being burdened by additional work outside of class.</p>
                     <h3>Example Student Presentation</h3>
-                    <img src={ppexample} alt="PeerPresents example"></img>
+                    <div className="expimages">
+                        <img src={ppexample} alt="PeerPresents example"></img>
+                    </div>
                     <h3>My Role</h3>
                     <p>Advise and help a team of students during the development of data display feature for instructor accounts on the PeerPresents website under the project leadership of Dr. Cook</p>
                     <ul>
                         <li>Developed presentation and pitched the project to classroom of computer science students at The University of Memphis to gather together a team of 3 other students.</li>
-                        <li>Served as an advisor to students working on the project by providing instruction, explaining codebase, and helping developers where needed. </li>
+                        <li>Served as an advisor and managed team of 3 students working on the project by providing instruction, explaining codebase, and helping developers where needed. </li>
                         <li>Took leadership by meeting weekly with Dr. Cook to communicate team progress and any requests from her back to the team, led weekly team code reviews, and reviewed team pull requests.</li>
                     </ul>
                     <h3>Outcome</h3>
-                    <img src={ppgraph} alt="PeerPresents Graph"></img>
+                    <div className="expimages">
+                        <img src={ppgraph} alt="PeerPresents Graph"></img>
+                    </div>
                     <p>Resulted in functioning data page for instructors where they can create datasets made up of specific student presentations to view student statistics. Data shown is based off chosen student presentations in the dataset and is displayed in an easy to read bar graph. Instructors are also able to change what kind of data they are viewing.</p>
                     <p className="highlight">Leveraged knowledge in: Full stack web development, microservice architecture, Git, React.js, JSX, HTML, CSS, Javascript, MongoDB.</p>
                 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,17 +12,20 @@ class Projects extends Component {
                 <a href="https://github.com/juliobarajas/English-Grammar-Parser" target="_blank" className="background">
                     <img src={parser} alt="parser" className="img1"></img>  
                     <div className="desc">
-                        <h3 className="highlight">English Grammar Parser</h3>
+                        <h3><i className="highlight">English Grammar Parser</i></h3>
                         <p>Built using: Python, HTML, CSS, Django, and Parsimonious Parser</p>
                     </div>
                 </a>  
                 <a href="https://github.com/juliobarajas/Coffeemaker-Project" target="_blank" className="background">
                     <img src={coffeemaker} alt="coffee maker" className="img2"></img>
                     <div className="desc">
-                        <h3 className="highlight">Coffee Maker Project</h3>
+                        <h3><i className="highlight">Coffee Maker Project </i></h3>
                         <p>Built using: Java, Object Oriented Programming</p>
                     </div>    
                 </a>   
+                <a href="https://github.com/juliobarajas/Comp-Java-Programs/tree/master/Collection" target="_blank" className="other">
+                    <h3><i className="highlight">Other Java Programs I've Written</i></h3>   
+                </a>
             </div>
         );
     }
